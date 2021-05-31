4ba95142e2bb7438884f81e5a729f485237da486
JDJG Inc. Official <>
2021-05-31 00:10:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord, os, random, asyncio
+from discord.ext import commands
+import ClientConfig, B 
+
+bot = ClientConfig.client
+
+@bot.listen()
+async def on_ready():
+  print("Bot is Ready")
+  print(f"Logged in as {bot.user}")
+  print(f"Id: {bot.user.id}")
+
+async def status_task():
+  await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name="Classic computer games."))
+  await asyncio.sleep(40)
+  await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(bot.guilds)} servers | {len(bot.users)} users"))
+  await asyncio.sleep(40)
+ 
+
+async def startup():
+  await bot.wait_until_ready()
+  await status_task()
+
+B.b()
+bot.loop.create_task(startup())
+bot.run(os.environ["TOKEN"])
