4861ee998ed50a5c708507a724cca64c64ba05cc
JDJG Inc. Official <ghostzap123@gmail.com>
2022-03-02 13:16:56 -0500

    Revert "Re-formatted % better stuff"

--- main.py
+++ main.py
@@ -1,43 +1,34 @@
 import discord, os, random, asyncio, traceback
-from discord.ext import commands, tasks
+from discord.ext import commands
 import ClientConfig
-from itertools import cycle
 import dotenv
 
-bot = ClientConfig.DaDiscordBot()
+bot = ClientConfig.client
 
-statuses = ["classic computer games.", "{len(0.guilds)} servers | {len(0.users)} users"]
-cycling = cycle(statuses)
-
-
-@tasks.loop(seconds=40.0)
-async def status_task():
-    status = next(cycling)
-    status = status.format(bot)
-    if status.startswith(f"{len(bot.guilds)}"):
-        await bot.change_presence(
-            status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=status)
-        )
-    await bot.change_presence(
-        status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name=status)
-    )
-
-
-@bot.event
+@bot.listen()
 async def on_ready():
-    print("Bot is Ready")
-    print(f"Logged in as {bot.user}")
-    print(f"Id: {bot.user.id}")
-    status_task.start()
+  print("Bot is Ready")
+  print(f"Logged in as {bot.user}")
+  print(f"Id: {bot.user.id}")
 
+async def status_task():
+  await bot.change_presence(status=discord.Status.online, activity = discord.Activity(type=discord.ActivityType.playing, name="Classic computer games."))
+  await asyncio.sleep(40)
+  await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type = discord.ActivityType.watching, name=f"{len(bot.guilds)} servers | {len(bot.users)} users"))
+  await asyncio.sleep(40)
+ 
+
+async def startup():
+  await bot.wait_until_ready()
+  await status_task()
 
 @bot.event
 async def on_error(event, *args, **kwargs):
-    more_information = os.sys.exc_info()
-    error_wanted = traceback.format_exc()
-    traceback.print_exc()
-    # print(more_information[0])
-
+  more_information = os.sys.exc_info()
+  error_wanted = traceback.format_exc()
+  traceback.print_exc()
+  #print(more_information[0])
 
 dotenv.load_dotenv()
+bot.loop.create_task(startup())
 bot.run(os.environ["TOKEN"])
